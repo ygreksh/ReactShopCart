55aaa13bbc1159b670eb7a49a8c0ca9230a06865
ygrek <ygreksh@gmail.com>
2022-02-28 13:45:19 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Routes, Switch} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Cart from './components/Cart'
+
+class App extends Component {
+  render() {
+    return (
+       <BrowserRouter>
+            <div className="App">
+            
+              <Navbar/>
+                  <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                  </Routes>
+             </div>
+       </BrowserRouter>
+      
+    );
+  }
+}
+
+export default App;
