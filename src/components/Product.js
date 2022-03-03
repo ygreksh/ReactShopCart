03d3e3d14262d5779011738e57839f5d92f4b28a
ygrek <ygreksh@gmail.com>
2022-03-03 15:23:59 +0200

    loading products with images from json

--- src/components/Product.js
+++ src/components/Product.js
@@ -23,9 +23,9 @@ const Product = (props) => {
 
         return (
             <div className="card" key={key}>
-                        <div className="card-image">
-                            <img src={item.img} alt={item.title} onClick={() => handleProductClick()}/>
-                            <span className="card-title">{item.title}</span>
+                        <div className="card-image" >
+                            <img src={item.img} alt={item.title} />
+                            <span className="card-title" onClick={() => handleProductClick()}>{item.title}</span>
                             <button to="/" className="btn-floating halfway-fab waves-effect waves-light red" onClick={()=>{handleAddClick()}}><i className="material-icons">add</i></button>
                         </div>
                         <div className="card-content">
